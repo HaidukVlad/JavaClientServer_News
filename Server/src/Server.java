e9839e4f3b66109e3dd6e386df4786aa9dab2832
vlad_haiduk <lolly1bomb2@gmail.com>
2024-11-01 00:18:36 +0300

    Rewrite with try() {} catch {} to do program and code clearer and optimised

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -4,26 +4,27 @@ import java.net.Socket;
 
 
 public class Server {
-    public static void main(String[] args) throws IOException {
-        ServerSocket server = new ServerSocket(8000);
-        System.out.println("Server starting...");
+    public static void main(String[] args) {
 
+        try (ServerSocket server = new ServerSocket(8000))
+        {
+            System.out.println("Server starting...");
 
-        Socket socket = server.accept();
-        System.out.println("Client Connected!");
+            Socket socket = server.accept();
+            System.out.println("Client Connected!");
 
+            BufferedWriter writer =
+                    new BufferedWriter(
+                            new OutputStreamWriter(
+                                    socket.getOutputStream()));
+            writer.write("HELLO FROM VLAD'S SERVER!");
+            writer.newLine();
+            writer.flush();
 
-        BufferedWriter writer =
-                new BufferedWriter(
-                        new OutputStreamWriter(
-                                socket.getOutputStream()));
-        writer.write("HELLO FROM VLAD'S SERVER!");
-        writer.newLine();
-        writer.flush();
-
-
-        writer.close();
-        socket.close();
-        server.close();
+            writer.close();
+            socket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
