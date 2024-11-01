fa662f106ef90aed66961e0711035f952be1c8f6
vlad_haiduk <lolly1bomb2@gmail.com>
2024-11-01 14:13:33 +0300

    Using creator in client.java

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,28 +1,19 @@
+import creator_package.Creator;
+
 import java.io.*;
 import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) {
-        try (
-                Socket socket = new Socket("127.0.0.1", 8000);
-                BufferedWriter writer =
-                        new BufferedWriter(
-                                new OutputStreamWriter(
-                                        socket.getOutputStream()));
-                BufferedReader reader =
-                        new BufferedReader(
-                                new InputStreamReader(
-                                        socket.getInputStream()))
-        )
+        try ( Creator creator = new Creator("127.0.0.1", 8000))
         {
             System.out.println("Client connected");
+
             String request = "Minsk";
             System.out.println("Request: " + request);
-            writer.write(request);
-            writer.newLine();
-            writer.flush();
+            creator.writeLine(request);
 
-            String response = reader.readLine();
+            String response = creator.readLine();
             System.out.println("Response:   " + response);
         } catch (IOException e) {
             e.printStackTrace();
